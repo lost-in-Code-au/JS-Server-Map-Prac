372740668911e10aaf027a1c654f299afafd8a40
lost-in-Code-au <reece.c.jones@gmail.com>
2017-10-30 15:39:44 +0000

    update of express

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
+  console.log(path)
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
